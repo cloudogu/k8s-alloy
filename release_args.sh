5401a7e585a2e1a4ae863ef68af8202981079103
Niklas <niklas.hinze@cloudogu.com>
2025-07-15 15:14:07 +0200

    #1 Implement release_args.sh

--- release_args.sh
+++ release_args.sh
@@ -3,67 +3,43 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-componentTemplateFile=k8s/helm/component-patch-tpl.yaml
-lokiTempChart="/tmp/loki"
-lokiTempValues="${lokiTempChart}/values.yaml"
-lokiTempChartYaml="${lokiTempChart}/Chart.yaml"
-
-lokiValues="k8s/helm/values.yaml"
+componentTemplateFile="k8s/helm/component-patch-tpl.yaml"
+alloyTempChart="/tmp/alloy"
+alloyTempValues="${alloyTempChart}/values.yaml"
+alloyTempChartYaml="${alloyTempChart}/Chart.yaml"
 
 # this function will be sourced from release.sh and be called from release_functions.sh
 update_versions_modify_files() {
   echo "Update helm dependencies"
   make helm-update-dependencies  > /dev/null
 
-  # Extract loki chart
-  local lokiVersion
-  lokiVersion=$(yq '.dependencies[] | select(.name=="loki").version' < "k8s/helm/Chart.yaml")
-  local lokiPackage
-  lokiPackage="k8s/helm/charts/loki-${lokiVersion}.tgz"
+  # Extract alloy chart
+  local alloyVersion
+  alloyVersion=$(yq '.dependencies[] | select(.name=="alloy").version' < "k8s/helm/Chart.yaml")
+  local alloyPackage
+  alloyPackage="k8s/helm/charts/alloy-${alloyVersion}.tgz"
 
-  echo "Extract loki helm chart"
-  tar -zxvf "${lokiPackage}" -C "/tmp" > /dev/null
+  echo "Extract alloy helm chart"
+  tar -zxvf "${alloyPackage}" -C "/tmp" > /dev/null
 
-  local lokiAppVersion
-  lokiAppVersion=$(yq '.appVersion' < "${lokiTempChartYaml}")
+  local alloyAppVersion
+  alloyAppVersion=$(yq '.appVersion' < "${alloyTempChartYaml}")
 
   echo "Set images in component patch template"
 
-  local lokiKubectlRegistry
-  local lokiKubectlRepo
-  local lokiKubectlTag
-  lokiKubectlRegistry=$(yq '.loki.kubectlImage.registry' < "${lokiValues}")
-  lokiKubectlRepo=$(yq '.loki.kubectlImage.repository' < "${lokiValues}")
-  lokiKubectlTag=$(yq '.loki.kubectlImage.tag' < "${lokiValues}")
-  setAttributeInComponentPatchTemplate ".values.images.kubectl" "${lokiKubectlRegistry}/${lokiKubectlRepo}:${lokiKubectlTag}"
+  local alloyImageRegistry
+  local alloyImageRepo
+  alloyImageRegistry=$(yq '.image.registry' < "${alloyTempValues}")
+  alloyImageRepo=$(yq '.image.repository' < "${alloyTempValues}")
+  setAttributeInComponentPatchTemplate ".values.images.alloy" "${alloyImageRegistry}/${alloyImageRepo}:${alloyAppVersion}"
 
-  local lokiImageRegistry
-  local lokiImageRepo
-  lokiImageRegistry=$(yq '.loki.image.registry' < "${lokiTempValues}")
-  lokiImageRepo=$(yq '.loki.image.repository' < "${lokiTempValues}")
-  setAttributeInComponentPatchTemplate ".values.images.loki" "${lokiImageRegistry}/${lokiImageRepo}:${lokiAppVersion}"
-
-  local lokiCanaryRegistry
-  local lokiCanaryRepo
-  lokiCanaryRegistry=$(yq '.lokiCanary.image.registry' < "${lokiTempValues}")
-  lokiCanaryRepo=$(yq '.lokiCanary.image.repository' < "${lokiTempValues}")
-  setAttributeInComponentPatchTemplate ".values.images.lokiCanary" "${lokiCanaryRegistry}/${lokiCanaryRepo}:${lokiAppVersion}"
-
-  local lokiGatewayRegistry
-  local lokiGatewayRepo
-  local lokiGatewayTag
-  lokiGatewayRegistry=$(yq '.gateway.image.registry' < "${lokiTempValues}")
-  lokiGatewayRepo=$(yq '.gateway.image.repository' < "${lokiTempValues}")
-  lokiGatewayTag=$(yq '.gateway.image.tag' < "${lokiTempValues}")
-  setAttributeInComponentPatchTemplate ".values.images.gateway" "${lokiGatewayRegistry}/${lokiGatewayRepo}:${lokiGatewayTag}"
-
-  local lokiSidecarRegistryRepo
-  local lokiSidecarTag
-  lokiSidecarRegistryRepo=$(yq '.sidecar.image.repository' < "${lokiTempValues}")
-  lokiSidecarTag=$(yq '.sidecar.image.tag' < "${lokiTempValues}")
-  setAttributeInComponentPatchTemplate ".values.images.sidecar" "${lokiSidecarRegistryRepo}:${lokiSidecarTag}"
-
-  rm -rf ${lokiTempChart}
+  local alloyConfigReloadRegistry
+  local alloyConfigReloadRepo
+  local alloyConfigReloadTag
+  alloyConfigReloadRegistry=$(yq '.configReloader.image.registry' < "${alloyTempValues}")
+  alloyConfigReloadRepo=$(yq '.configReloader.image.repository' < "${alloyTempValues}")
+  alloyConfigReloadTag=$(yq '.configReloader.image.tag' < "${alloyTempValues}")
+  setAttributeInComponentPatchTemplate ".values.images.configReloader" "${alloyConfigReloadRegistry}/${alloyConfigReloadRepo}:${alloyConfigReloadTag}"
 }
 
 setAttributeInComponentPatchTemplate() {
