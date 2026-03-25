d7026286649b6cda6bbf5f84e067cb1ec55d0b09
Robert Auer <robert.auer@cloudogu.com>
2026-03-25 13:32:59 +0100

    Get custom image settings for component patch
    As we overwrite the alloy and config-reloader image versions
    in the values.yaml, we should use these versions for the
    component patch file instead of the original chart versions.
    The versions from the original alloy helm chart are outdated.
    
    This could also be resolved by using the most recent alloy helm chart,
    but with this solution we are a bit more flexible to adjust the
    image versions to our needs.

--- release_args.sh
+++ release_args.sh
@@ -4,41 +4,24 @@ set -o nounset
 set -o pipefail
 
 componentTemplateFile="k8s/helm/component-patch-tpl.yaml"
-alloyTempChart="/tmp/alloy"
-alloyTempValues="${alloyTempChart}/values.yaml"
-alloyTempChartYaml="${alloyTempChart}/Chart.yaml"
 
 # this function will be sourced from release.sh and be called from release_functions.sh
 update_versions_modify_files() {
-  echo "Update helm dependencies"
-  make helm-update-dependencies  > /dev/null
-
-  # Extract alloy chart
-  local alloyVersion
-  alloyVersion=$(./.bin/yq '.dependencies[] | select(.name=="alloy").version' < "k8s/helm/Chart.yaml")
-  local alloyPackage
-  alloyPackage="k8s/helm/charts/alloy-${alloyVersion}.tgz"
-
-  echo "Extract alloy helm chart"
-  tar -zxvf "${alloyPackage}" -C "/tmp" > /dev/null
-
-  local alloyAppVersion
-  alloyAppVersion=$(./.bin/yq '.appVersion' < "${alloyTempChartYaml}")
-
   echo "Set images in component patch template"
 
   local alloyImageRegistry
   local alloyImageRepo
-  alloyImageRegistry=$(./.bin/yq '.image.registry' < "${alloyTempValues}")
-  alloyImageRepo=$(./.bin/yq '.image.repository' < "${alloyTempValues}")
+  alloyAppVersion=$(./.bin/yq '.alloy.image.tag' < "k8s/helm/values.yaml")
+  alloyImageRegistry=docker.io
+  alloyImageRepo=grafana/alloy
   setAttributeInComponentPatchTemplate ".values.images.alloy" "${alloyImageRegistry}/${alloyImageRepo}:${alloyAppVersion}"
 
   local alloyConfigReloadRegistry
   local alloyConfigReloadRepo
   local alloyConfigReloadTag
-  alloyConfigReloadRegistry=$(./.bin/yq '.configReloader.image.registry' < "${alloyTempValues}")
-  alloyConfigReloadRepo=$(./.bin/yq '.configReloader.image.repository' < "${alloyTempValues}")
-  alloyConfigReloadTag=$(./.bin/yq '.configReloader.image.tag' < "${alloyTempValues}")
+  alloyConfigReloadRegistry=quay.io
+  alloyConfigReloadRepo=prometheus-operator/prometheus-config-reloader
+  alloyConfigReloadTag=$(./.bin/yq '.alloy.configReloader.image.tag' < "k8s/helm/values.yaml")
   setAttributeInComponentPatchTemplate ".values.images.configReloader" "${alloyConfigReloadRegistry}/${alloyConfigReloadRepo}:${alloyConfigReloadTag}"
 }
 
